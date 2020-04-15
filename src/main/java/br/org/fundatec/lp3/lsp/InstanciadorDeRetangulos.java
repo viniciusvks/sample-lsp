46ca6161f8c4217eaba584f69c996a8554ae4a62
Vinicius <viniciusvks@gmail.com>
2020-04-14 22:26:45 -0300

    Removidas classes primeira versao

--- src/main/java/br/org/fundatec/lp3/lsp/InstanciadorDeRetangulos.java
+++ src/main/java/br/org/fundatec/lp3/lsp/InstanciadorDeRetangulos.java
@@ -1,18 +1,9 @@
 package br.org.fundatec.lp3.lsp;
 
 public class InstanciadorDeRetangulos {
-
-	private int ultimoObjeto = 0;
-
+	
 	public Retangulo novoRetangulo() {
-
-		if(ultimoObjeto == 0) {
-			ultimoObjeto = 1;
-			return new Retangulo();
-		} else {
-			ultimoObjeto = 0;
-			return new Quadrado();
-		}
+		return new Retangulo();
 	}
 
 }
--- src/test/java/br/org/fundatec/lp3/lsp/InstanciadorDeRetangulosTest.java
+++ src/test/java/br/org/fundatec/lp3/lsp/InstanciadorDeRetangulosTest.java
@@ -8,39 +8,16 @@ public class InstanciadorDeRetangulosTest {
 
 	@Test
 	public void testAreaRetangulo() {
-
+		
 		InstanciadorDeRetangulos gerador = new InstanciadorDeRetangulos();
-
 		Retangulo retangulo = gerador.novoRetangulo();
-
+		
 		retangulo.setLargura(10);
 		retangulo.setAltura(5);
-
+		
 		int areaEsperada = 50;
 
 		assertEquals(areaEsperada, retangulo.getArea());
-
-	}
-
-	@Test
-	public void testAreaDoisRetangulos() {
-
-		InstanciadorDeRetangulos gerador = new InstanciadorDeRetangulos();
-
-		Retangulo primeiroRetangulo = gerador.novoRetangulo();
-		Retangulo segundoRetangulo = gerador.novoRetangulo();
-
-		primeiroRetangulo.setLargura(10);
-		primeiroRetangulo.setAltura(5);
-
-		segundoRetangulo.setLargura(6);
-		segundoRetangulo.setAltura(5);
-
-		int areaEsperadaPrimeiroRetangulo = 50;
-		int areaEsperadaSegundoRetangulo = 30;
-
-		assertEquals(areaEsperadaPrimeiroRetangulo, primeiroRetangulo.getArea());
-		assertEquals(areaEsperadaSegundoRetangulo, segundoRetangulo.getArea());
 
 	}
 
